0e4cb4a92473cf61e5df6d5aff3714a0849be537
danielhinker <danhinker@gmail.com>
2020-11-19 23:26:00 -0500

    Add react components and cors

--- posts/index.js
+++ posts/index.js
@@ -1,8 +1,12 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
-const app = express()
-app.use(bodyParser.json());
 const { randomBytes } = require('crypto');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
 const posts = {};
 
 app.get('/posts', (req, res) => {
