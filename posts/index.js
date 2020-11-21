f7df48f21c8ce4be7f2938f9647a5df2d470fd31
danielhinker <danhinker@gmail.com>
2020-11-20 23:52:04 -0500

    Update index.js

--- posts/index.js
+++ posts/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require('express')
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
 
-const app = express();
+const app = express()
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -18,7 +18,7 @@ app.post('/posts', (req, res) => {
     const { title } = req.body;
 
     posts[id] = {
-        id, title
+        id, title 
     };
 
     res.status(201).send(posts[id]); 
